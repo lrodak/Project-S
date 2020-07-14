48f1b7ec3bf22f0f1d8779ceaa60eccd75855c9e
RodakS <61910339+RodakS@users.noreply.github.com>
2020-07-14 10:34:17 +0200

    Revert "Pierwszy commit"
    This reverts commit 52300c313f56a4a6322c5dddb390675c7c9da160.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("loginPanel.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
