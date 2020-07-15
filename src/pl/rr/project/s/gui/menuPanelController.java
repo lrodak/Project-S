09aaecf4e2bd0151f745f717813fc510e6725275
RodakS <61910339+RodakS@users.noreply.github.com>
2020-07-15 11:45:07 +0200

    First button

--- src/pl/rr/project/s/gui/menuPanelController.java
+++ src/pl/rr/project/s/gui/menuPanelController.java
@@ -1,15 +1,20 @@
 package pl.rr.project.s.gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class menuPanelController {
+    @FXML
+    private Button playButton;
     public void goToProfile(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/profilePanel.fxml"));
         Scene scene = new Scene(root);
@@ -18,5 +23,9 @@ public class menuPanelController {
         window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
+    }
+
+    public void onMouseCarried(MouseEvent mouseEvent){
+        playButton.setTooltip(new Tooltip("Tooltip for Button"));
     }
 }
