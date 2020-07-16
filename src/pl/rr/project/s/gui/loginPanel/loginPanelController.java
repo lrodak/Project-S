0d7cc935618e2f96524ecfbca2b13c26245f08b8
rodakA <diabe77@gmail.com>
2020-07-16 16:27:43 +0200

    loginPanel check if username and password is empty

--- src/pl/rr/project/s/gui/loginPanel/loginPanelController.java
+++ src/pl/rr/project/s/gui/loginPanel/loginPanelController.java
@@ -2,15 +2,34 @@ package pl.rr.project.s.gui.loginPanel;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class loginPanelController extends loginPanelMethods {
 
     @FXML
-    public void goToMenu(MouseEvent mouseEvent) throws IOException {
-        goToScene("/fxml/menuPanel.fxml", null, mouseEvent);
+     private javafx.scene.control.TextField username_ID;
+    @FXML
+     private PasswordField password_ID;
+    @FXML
+    private Label invalid;
+
+    @FXML
+    public void login(MouseEvent mouseEvent) throws IOException {
+
+        if(username_ID.getText().isEmpty() || password_ID.getText().isEmpty())
+        {
+            invalid.setVisible(true);
+        }
+        else
+        {
+            goToScene("/fxml/menuPanel.fxml", null, mouseEvent);
+        }
     }
 
     @FXML
--- src/pl/rr/project/s/gui/registerPanel/registerPanelController.java
+++ src/pl/rr/project/s/gui/registerPanel/registerPanelController.java
@@ -2,6 +2,8 @@ package pl.rr.project.s.gui.registerPanel;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
@@ -9,8 +11,31 @@ import java.io.IOException;
 public class registerPanelController extends registerPanelMethods{
 
     @FXML
+    private javafx.scene.control.TextField username_ID;
+    @FXML
+    private PasswordField password_ID;
+    @FXML
+    private Label invalid;
+
+    @FXML
+    public void register(MouseEvent mouseEvent) throws IOException
+    {
+        if(username_ID.getText().isEmpty() || password_ID.getText().isEmpty())
+        {
+            invalid.setVisible(true);
+        }
+        else
+        {
+            goToScene("/fxml/loginPanel.fxml", null, mouseEvent);
+        }
+
+    }
+
+
+
+    @FXML
     public void goLoginPanel(MouseEvent mouseEvent) throws IOException {
-        goToScene("/fxml/menuPanel.fxml", null, mouseEvent);
+        goToScene("/fxml/loginPanel.fxml", null, mouseEvent);
     }
 
     @FXML
