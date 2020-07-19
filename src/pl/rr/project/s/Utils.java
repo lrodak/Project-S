ef2988a891b875d200469c4be9c23e68553ded6d
MateuszRodak <mateusz.rodak108@gmail.com>
2020-07-19 15:16:55 +0200

    deleted comments adding password remembering

--- src/pl/rr/project/s/Utils.java
+++ src/pl/rr/project/s/Utils.java
@@ -7,10 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+
 
 //universal methods for multi-purpose
-public class Utils {
+public class Utils<daysOfWeek, items> {
 
     public void goToScene(String sceneName, Stage stage, MouseEvent event) throws IOException {
         Parent root = null;
@@ -31,4 +32,31 @@ public class Utils {
         window.setScene(scene);
         window.show();
     }
+
+
+
+
+
+
+
+    public void savePassword() {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("Passy.data"));
+            outputStream.writeObject(Main.autoLoginObject);
+        } catch (IOException ex) {
+            System.out.println("Saving error");
+            ex.printStackTrace();
+        }
+    }
+
+    public void readPassword() {
+        try {
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("Passy.data"));
+            Object autoLoginObject = inputStream.readObject();
+        } catch (Exception ex) {
+            System.out.println("Reading error");
+            ex.printStackTrace();
+        }
+    }
+
 }
